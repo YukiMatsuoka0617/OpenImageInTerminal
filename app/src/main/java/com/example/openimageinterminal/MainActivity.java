e43afdfd3b67652b31c8fa2fa47c8da0ca83e0db
YukiMATSUOKA <yumitukai.yuki@gmail.com>
2021-10-08 00:10:53 +0900

    Open image in terminal

---
+++ app/src/main/java/com/example/openimageinterminal/MainActivity.java
@@ -0,0 +1,75 @@
+package com.example.openimageinterminal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.ParcelFileDescriptor;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.io.FileDescriptor;
+import java.io.IOException;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity {
+    private static final int RESULT_PICK_IMAGEFILE = 1001;
+    private TextView textView;
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.text_view);
+
+        imageView = findViewById(R.id.image_view);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener( v -> {
+            Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+            intent.addCategory(Intent.CATEGORY_OPENABLE);
+            intent.setType("*/*");
+            startActivityForResult(intent, RESULT_PICK_IMAGEFILE);
+        });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent resultData) {
+        super.onActivityResult(requestCode, resultCode, resultData);
+        if (requestCode == RESULT_PICK_IMAGEFILE && resultCode == Activity.RESULT_OK) {
+            if(resultData.getData() != null){
+                ParcelFileDescriptor pfDescriptor = null;
+                try{
+                    Uri uri = resultData.getData();
+                    textView.setText(
+                            String.format(Locale.US, "Uri:　%s",uri.toString()));
+                    pfDescriptor = getContentResolver().openFileDescriptor(uri, "r");
+                    if(pfDescriptor != null){
+                        FileDescriptor fileDescriptor = pfDescriptor.getFileDescriptor();
+                        Bitmap bmp = BitmapFactory.decodeFileDescriptor(fileDescriptor);
+                        pfDescriptor.close();
+                        imageView.setImageBitmap(bmp);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                } finally {
+                    try{
+                        if(pfDescriptor != null){
+                            pfDescriptor.close();
+                        }
+                    }catch (Exception e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }
+    }
+    
+}
